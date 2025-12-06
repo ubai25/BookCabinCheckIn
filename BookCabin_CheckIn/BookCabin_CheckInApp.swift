7daf0f174bf5ea568bbfcf5b7db0ddbdce191aac
ubai <a.ubai25@gmail.com>
2025-12-07 05:43:36 +0700

    Initial Commit

---
+++ BookCabin_CheckIn/BookCabin_CheckInApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookCabin_CheckInApp.swift
+//  BookCabin_CheckIn
+//
+//  Created by Ubaidillah Ahmad on 07/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BookCabin_CheckInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
